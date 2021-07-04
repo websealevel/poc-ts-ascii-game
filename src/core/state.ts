130d76e9e0922cab4e2c2b693c7b1ed1ba83af6c
Paul Schuhmacher <pschuhmacher@studio-goliath.com>
2021-07-04 23:24:33 +0200

    config etat global canvas (avant de mettre un store en place plus propre)

--- src/core/state.ts
+++ src/core/state.ts
@@ -1,5 +1,9 @@
 import createCanvas from './canvas'
 
+/**
+ * Etat global du jeu (A déplacer dans un store(transaction) plus tard)
+ */
+
 // Canvas : fenetre de rendu graphique de l'état du jeu/zone d'interaction avec le joueur
 const canvas: HTMLCanvasElement = createCanvas(document)
 export default canvas
